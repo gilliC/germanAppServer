54093db3600143c141196f008ff566e8744415e5
gilliC <gillicarmon@gmail.com>
2018-04-21 18:02:21 +0300

    bigger DB

--- server/models.py
+++ server/models.py
@@ -10,5 +10,5 @@ class Word(models.Model):
     id = models.IntegerField(primary_key=True)
     german_word = models.CharField(max_length=250)
     english_translation = models.CharField(max_length=250)
-    gender = models.CharField(max_length=4, default="der")
+    gender = models.CharField(max_length=4, default="Der")
 
