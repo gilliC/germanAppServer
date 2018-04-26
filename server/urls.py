3850be1df810ed3381776e3ff371b3a8855c365c
gilliC <gillicarmon@gmail.com>
2018-04-26 21:28:09 +0300

    adding the option to insert & delete

--- server/urls.py
+++ server/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.get_data, name='get_data'),
+    url(r'^insert/(?P<german_word>[\w\-]+)/(?P<english_translation>[\w\-]+)/(?P<gender>[\w\-]+)/$', views.insert_word, name='insert_word'),
+    url(r'^delete/(?P<word_id>\d+)/$', views.delete_word, name='delete_word'),
+
 ]
--- server/views.py
+++ server/views.py
@@ -1,12 +1,37 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.core.serializers import serialize
 import json
 
 from .models import Word
 
 
-def index(request):
-        vocabulary = Word.objects.all()
-        json_data = serialize("json", vocabulary)
-        final_data = json.loads(json_data)
-        return JsonResponse(final_data, safe=False)
+def get_data(request):
+    vocabulary = Word.objects.all()
+    json_data = serialize("json", vocabulary)
+    final_data = json.loads(json_data)
+    return JsonResponse(final_data, safe=False)
+
+
+def insert_word(request, german_word, english_translation, gender):
+    if Word.objects.filter(german_word=german_word.title()).exists():
+        answer = "WordExists"
+    else:
+        try:
+            new_word = Word(german_word=german_word.title(), english_translation=english_translation.title(), gender=gender.title())
+            new_word.save()
+            answer = "Succeed"
+        except ValueError:
+            answer = "Failed"
+    return JsonResponse(answer, safe=False)
+
+
+def delete_word(request, word_id):
+    answer = "Failed"
+    try:
+        word_deleting = Word.objects.get(pk=word_id)
+        word_deleting.delete()
+        answer = "Deleted: " + word_id
+    except ValueError:
+        answer = "Failed"
+    finally:
+        return JsonResponse(answer, safe=False)
